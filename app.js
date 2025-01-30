d5052544a97e080491adf050a012832375a8262c
ridaDev <kiratterida@gmail.com>
2025-01-30 16:47:50 +0100

    ajouter function affichier

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const taskTitle = document.getAnimations("taskTitle");
-const list = [];
+const list = JSON.parse(localStorage.getItem("list")) || [];
 const forml = document.querySelector("form");
 let termine = document.getElementById("terminé");
 document.getElementById("ajouter").addEventListener("click", AddTache);
 termine.addEventListener("click", terminé);
+const taskList = document.getElementById("crud");
+
+
 function AddTache() {    
     forml.classList.remove("d-none");
 
@@ -20,9 +23,42 @@ function terminé() {
         list.push(data);
         
         localStorage.setItem("notes", JSON.stringify(list));
-        deleteinput();
+        document.getElementById('form').reset();
+        
     } else {
         console.log("Please fill in all fields.");
     }
+    window.location.href="index.html";
 }
+
+
+function AfficheTache() {
+    console.log("afficher");
+    
+    const data = JSON.parse(localStorage.getItem("notes"));
+  
+
+    if (data) {
+        // list.push(data);
+        // console.log("list"+list);
+        data.forEach((dt, index) => {
+            const task =innerHTML = `
+            <div class="bg-white row d-flex justify-content-between align-items-center">
+            <div class="col-6">
+            <h3>${dt.titre}</h3>
+            <p>${dt.description}</p>
+            <p>${dt.Priorité}</p>
+            </div>
+            <div class="col-6">
+            <button class="btn btn-danger" onclick="deleteTask(${index})">Delete</button>
+            <button class="btn btn-warning"  onclick="editTask(${index})">Edit</button>
+            </div>
+            
+            </div>
+            `;
+            taskList.insertAdjacentHTML("beforeend",task);
+        });
+    }
+}
+window.onload = AfficheTache;
 
