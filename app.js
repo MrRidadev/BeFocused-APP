e745c301396b77320cac8653b860af01b9052b3f
ridaDev <kiratterida@gmail.com>
2025-01-30 12:32:59 +0100

    ajouter function add

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-forml = document.querySelector("form");
-function AddTache(){
-  
-forml.classList.remove("d-none")
+const taskTitle = document.getAnimations("taskTitle");
+const list = [];
+const forml = document.querySelector("form");
+let termine = document.getElementById("terminé");
+document.getElementById("ajouter").addEventListener("click", AddTache);
+termine.addEventListener("click", terminé);
+function AddTache() {    
+    forml.classList.remove("d-none");
+
 }
-function terminé(){
-    forml.classList.add("d-none")
+
+function terminé() {
+    forml.classList.add("d-none");
+    let titre = document.getElementById("taskTitle").value.trim();
+    let description = document.getElementById("taskDescription").value.trim();
+    let Priorité = document.getElementById("taskPriority").value.trim();
+ 
+    if (titre && description && Priorité) {
+        const data = { titre, description, Priorité };
+        list.push(data);
+        
+        localStorage.setItem("notes", JSON.stringify(list));
+        deleteinput();
+    } else {
+        console.log("Please fill in all fields.");
+    }
 }
+
